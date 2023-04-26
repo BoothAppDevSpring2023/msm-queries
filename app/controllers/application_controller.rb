b49feff799de1fd2b8bb5474cbae51fa8e361632
Mattia Lanzi <mlanzi@chicagobooth.edu>
2023-04-26 00:37:46 +0000

    v2

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -2,4 +2,41 @@ class ApplicationController < ActionController::Base
   def homepage
     render({ :template => "misc_templates/home"})
   end
+  def directors
+    @list_of_directors = Director.all
+    render({ :template => "director_templates/index"})
+  end
+  def wisest
+    @oldest= Director.where.not( { :dob => nil }).order({ :dob => :asc }).at(0)
+    render({ :template => "director_templates/eldest"})
+  end
+  def youngest
+    @youngest= Director.where.not( { :dob => nil }).order({ :dob => :desc }).at(0)
+    render({ :template => "director_templates/youngest"})
+  end
+  def director_details
+    the_id = params.fetch("an_id")
+    @the_director = Director.where({ :id => the_id}).at(0)
+    @filmography = Movie.where({ :director_id => @the_director.id })
+    render({ :template => "director_templates/show"})
+  end
+  def movies
+    @list_of_movies = Movie.all
+    render({ :template => "movies_templates/index"})
+  end
+  def movie_details
+    the_id = params.fetch("an_id")
+    @the_movie = Movie.where({ :id => the_id}).at(0)
+    render({ :template => "movies_templates/show"})
+  end
+  def actors
+    @list_of_actors = Actor.all
+    render({ :template => "actor_templates/index"})
+  end
+  def actor_details
+    the_id = params.fetch("an_id")
+    @the_actor = Actor.where({ :id => the_id}).at(0)
+    @characters = Character.where({ :actor_id => @the_actor.id })
+    render({ :template => "actor_templates/show"})
+  end
 end
--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,11 @@
 Rails.application.routes.draw do
-  get("/directors/eldest", { :controller => "directors", :action => "wisest" })
-  get("/directors/:an_id", { :controller => "directors", :action => "director_details" })
   get("/", { :controller => "application", :action => "homepage" })
-  get("/directors", { :controller => "directors", :action => "index" })
+  get("/directors", { :controller => "application", :action => "directors" })
+  get("/directors/eldest", { :controller => "application", :action => "wisest" })
+  get("/directors/youngest", { :controller => "application", :action => "youngest" })
+  get("/directors/:an_id", { :controller => "application", :action => "director_details" })
+  get("/movies", { :controller => "application", :action => "movies" })
+  get("/movies/:an_id", { :controller => "application", :action => "movie_details" })
+  get("/actors", { :controller => "application", :action => "actors" })
+  get("/actors/:an_id", { :controller => "application", :action => "actor_details" })
 end
